5802532a5485012a21fb0a2482e7ffb985691c9b
Ranganath-MD <ranganath.developer@gmail.com>
2020-10-31 22:40:22 -0700

    darkmode removed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,19 @@
 import Darkmode from 'darkmode-js'
 import kursor from "kursor"
 
-const options = {
-    left: '20px', 
-    time: '0.5s', 
-    mixColor: '#f8f8ff', 
-    buttonColorDark: "#fff",
-    buttonColorLight: '#100f2c',
-    saveInCookies: false,
-    label: "🌓", 
-    autoMatchOsTheme: true
-  }
+// const options = {
+//     left: '20px', 
+//     time: '0.5s', 
+//     mixColor: '#f8f8ff', 
+//     buttonColorDark: "#fff",
+//     buttonColorLight: '#100f2c',
+//     saveInCookies: false,
+//     label: "🌓", 
+//     autoMatchOsTheme: true
+//   }
   
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
+//   const darkmode = new Darkmode(options);
+//   darkmode.showWidget();
 
 
   // cursor type
